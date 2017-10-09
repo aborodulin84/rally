8d942e16e3c7519b2de1d2064a5ac329407b045a
aborodulin84 <aborodulin84@gmail.com>
2017-10-09 20:54:30 +0400

    1

---
+++ Assets/GameScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+    public static float speed = 0.01f;
+    public static bool go=true;
+    // Use this for initialization
+    
+}
---
+++ Assets/roaddown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roaddown : MonoBehaviour {
+   
+    [SerializeField]
+    private GameObject startFinish;
+    [SerializeField]
+   private GameObject Manager;
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(0, transform.position.y - 1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameScript.go == true)
+        {
+            transform.position = new Vector3(0, transform.position.y - GameScript.speed, 0);
+        }
+        if (transform.position.y < -10)
+        {
+            transform.position = new Vector3(0, 10, 0);
+            GameScript.speed *= 2;
+        }
+	}
+}
